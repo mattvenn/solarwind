8855f7fa8546b029b45e9ceff3097bd182822887
Matt Venn <matt@mattvenn.net>
2013-07-11 10:02:55 +0100

    solar wind to xively

---
+++ solarwind_to_xively.py
@@ -0,0 +1,32 @@
+import requests
+from CosmFeedUpdate import *
+
+url = 'http://www.swpc.noaa.gov/ftpdir/lists/ace/ace_swepam_1m.txt'
+feed_id = "1466087133"
+
+def fetch_speed():
+    r = requests.get(url)
+    if r.status_code == 200:
+        text = r.text.splitlines()
+        for line in reversed(text):
+            fields = line.split()
+            if int(fields[6]) == 0:
+                return(fields[0:4],fields[8])
+                break
+            else:
+                print "skipping bad line"
+
+(date,speed) = fetch_speed()
+print "current solar wind speed: ", speed
+
+#private key stored in a file
+keyfile="api.key"
+key=open(keyfile).readlines()[0].strip()
+
+pfu = CosmFeedUpdate(feed_id,key)
+pfu.addDatapoint('speed', speed)
+
+# finish up and submit the data
+pfu.buildUpdate()
+pfu.sendUpdate()
+
